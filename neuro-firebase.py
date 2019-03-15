bd7ad85f05f4d76ed8608f97f9e24441adb1048f
Tushar Sadana <tushar.169107181@muj.manipal.edu>
2019-03-15 16:24:22 +0530

    Added all the values to the firebase python file

--- neuro-firebase.py
+++ neuro-firebase.py
@@ -3,17 +3,27 @@ import json
 from NeuroPy import NeuroPy
 from time import sleep
 
-neuropy = NeuroPy("COM5")
-neuropy.connect()
+neuropy = NeuroPy("COM11")
+neuropy.start()
 
 firebase = 'https://eaisociety-1841c.firebaseio.com/brainwaves/aashis/.json'
 
 try:
     while True:
-        print(neuropy.raw_value)
-        firebase_value = json.dumps({'attention': neuropy.attention, 'meditation': neuropy.meditation,'rawvalue':neuropy.raw_value})
+        firebase_value = json.dumps({'attention': neuropy.attention,
+         'meditation': neuropy.meditation,
+         'rawValue': neuropy.rawValue,
+         'delta': neuropy.delta,
+         'theta': neuropy.theta,
+         'lowAlpha': neuropy.lowAlpha,
+         'highAlpha': neuropy.highAlpha,
+         'lowBeta': neuropy.lowBeta,
+         'highBeta': neuropy.highBeta,
+         'lowGamma': neuropy.lowGamma,
+         'midGamma': neuropy.midGamma})
         r = requests.put(url = firebase, data = firebase_value)
         sleep(5)
+        print(r.status_code)
 
 finally:
-    neuropy.disconnect()
+    neuropy.stop()
